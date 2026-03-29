ea56a6c2bcb43b937bd756f9d4adce0223692b9f
roaming <wilhelmus4321@gmail.com>
2026-03-29 12:03:00 +0100

    add home-manager backupFileExtension for conflict resolution

--- home/default.nix
+++ home/default.nix
@@ -3,6 +3,7 @@
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "hm-backup";
 
   home-manager.users.roaming = {
     imports = [
