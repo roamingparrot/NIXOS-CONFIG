f4b9c4a9842bac161e6583b27a844407add6893f
roaming <wilhelmus4321@gmail.com>
2026-03-29 12:10:33 +0100

    fix git credential helper conflict
    Let programs.gh.enable manage the credential helper automatically
    instead of hardcoding the path.

--- home/coding.nix
+++ home/coding.nix
@@ -16,14 +16,6 @@
     userName = "roaming";
     userEmail = "wilhelmus4321@gmail.com";
     extraConfig.init.defaultBranch = "main";
-    extraConfig.credential."https://github.com" = {
-      helper = "";
-      helper = "!/etc/profiles/per-user/roaming/bin/gh auth git-credential";
-    };
-    extraConfig.credential."https://gist.github.com" = {
-      helper = "";
-      helper = "!/etc/profiles/per-user/roaming/bin/gh auth git-credential";
-    };
   };
 
   programs.gh.enable = true;
