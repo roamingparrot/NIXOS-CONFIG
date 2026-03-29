1df20a4f85ba2a45543555259f2c22e9ed1486de
roaming <wilhelmus4321@gmail.com>
2026-03-29 12:08:03 +0100

    manage git and gh config through home-manager
    Moves .gitconfig into declarative home-manager config so credential
    helper paths stay correct as packages move between profiles.

--- home/coding.nix
+++ home/coding.nix
@@ -3,8 +3,6 @@
 {
   home.packages = with pkgs; [
     vscode
-    git
-    gh
     neovim
     opencode
     unzip
@@ -12,4 +10,21 @@
     windsurf
     tree
   ];
+
+  programs.git = {
+    enable = true;
+    userName = "roaming";
+    userEmail = "wilhelmus4321@gmail.com";
+    extraConfig.init.defaultBranch = "main";
+    extraConfig.credential."https://github.com" = {
+      helper = "";
+      helper = "!/etc/profiles/per-user/roaming/bin/gh auth git-credential";
+    };
+    extraConfig.credential."https://gist.github.com" = {
+      helper = "";
+      helper = "!/etc/profiles/per-user/roaming/bin/gh auth git-credential";
+    };
+  };
+
+  programs.gh.enable = true;
 }
